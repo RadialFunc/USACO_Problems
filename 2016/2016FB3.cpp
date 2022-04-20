19eed4b922b0d431f795d74f82edbf046aa77e31
RadialFunc <rohangarg2008@gmail.com>
2022-04-19 17:37:10 -0700

    fixed time issues for 2016fb3

--- 2016/2016FB3.cpp
+++ 2016/2016FB3.cpp
@@ -2,15 +2,16 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int> > cows;
+vector <pair<int, int> > cows;
+vector <int> x_cors, y_cors;
 
 int calculateleast(int a, int b, int n){
     int x=0; int y=0; int z=0; int w=0;
     for(int i=0; i<n; i++){
-        if(cows[i][0] > a && cows[i][1]>b) x++;
-        if(cows[i][0] > a && cows[i][1]<b) y++;
-        if(cows[i][0] < a && cows[i][1]>b) z++;
-        if(cows[i][0] < a && cows[i][1]<b) w++;
+        if(cows[i].first > a && cows[i].second>b) x++;
+        if(cows[i].first > a && cows[i].second<b) y++;
+        if(cows[i].first < a && cows[i].second>b) z++;
+        if(cows[i].first < a && cows[i].second<b) w++;
     }
     int p,q, r;
     return max(max(x,y), max(z,w));
@@ -20,26 +21,22 @@ int main(){
     freopen("balancing.in", "r", stdin);
     freopen("balancing.out", "w", stdout);
     int n,k;
-    cin >> n>>k;
-    int leastx=k;
-    int leasty = k;
-    int highestx = 0;
-    int highesty =0; 
+    cin >> n;
+    
+    int least = n;
     for(int i=0; i<n; i++){
         int x, y;
         cin >> x>>y;
-        vector<int> cow;
-        cow.push_back(x); cow.push_back(y); cows.push_back(cow);
-        if (x<leastx) leastx = x;
-        if(x>highestx) highestx =x;
-        if (y<leasty) leasty = y;
-        if(y>highesty) highesty =y; 
+		x_cors.push_back(x - 1);
+		y_cors.push_back(y - 1);
+		pair <int, int> coordinates (x, y);
+		cows.push_back(coordinates);
     }
-    int least=n;
-    for(int a=leastx-1; a<=highestx+1; a+=2){
-        for(int b=leasty-1; b<=highesty+1; b+=2){
-            int val =calculateleast(a,b, n);
-            if(val<least) least=val;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+			int a = x_cors[i];
+            int b = y_cors[j];
+            least = min(least, calculateleast(a,b,n));
         }
     }
     cout << least;
