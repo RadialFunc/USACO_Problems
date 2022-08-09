ed67f33e551ae6cae8533034d34e131bb3ddeb85
RadialFunc <rohangarg2008@gmail.com>
2022-08-09 13:08:17 -0700

    almost done w 2021mb1

--- 2021/2021MB1.cpp
+++ 2021/2021MB1.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
 #include <algorithm>
+#include <set>
+#include <vector>
 using namespace std;
+
+int comphindex(vector<int> papers, int n){
+    for(int k=0; k<n; k++){
+        if(k+1>= papers[k]) return papers[k];
+    }
+    return 0;
+}
 
 int main(){
     int n,l;
-    cin >> n>> l;
-    int papers[n];
-    int highesth = 0;
-    int highestcit = 0;
-    for(int i=0; i<n; i++){cin >> papers[i]; if(papers[i]>highestcit) highestcit = papers[i];}
-    sort(papers, papers+n);
-    for(int k=0; k<=highestcit; k++){
-        int vs[2];
-        int count=0;
-        int lastl = -1;
-        for(int i=0; i<n; i++){
-            if(papers[i] == k-1) count++;
-            if(papers[i] > k-1 && lastl ==-1) lastl = i;
-        }
-        vs[0] = count;
-        vs[1] = n-lastl;
-        if(vs[1] == n+1){
-            vs[1]=0;
-        }
-        int neee = min(vs[0],l);
-        if((neee + vs[1]) >= k){highesth = k;}
+    cin >> n >> l;
+    vector<int> papers;
+    multiset<int> papers2;
+    for(int i=0; i<n; i++){int k; cin >> k; papers.push_back(k); papers2.insert(k);}
+    sort(papers.begin(), papers.end(), greater<int>());
+    int h = comphindex(papers, n); 
+    int amt = papers2.count(h);
+    for(int i=0; i<min(l,amt); i++) papers.push_back(h+1);
 
-    }
-    if(highestcit==0){
-        cout << 1;
-    }else{
-    cout << highesth;}
+    sort(papers.begin(), papers.end(), greater<int>());
+    cout << comphindex(papers, papers.size());
 }
