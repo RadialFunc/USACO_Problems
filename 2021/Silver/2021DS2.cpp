10bbeb6485483057a2f6c13b65374e7f9d0fc59e
RadialFunc <rohangarg2008@gmail.com>
2022-10-22 18:41:29 -0700

    solved 2021ds2

--- 2021/Silver/2021DS2.cpp
+++ 2021/Silver/2021DS2.cpp
@@ -5,7 +5,7 @@ int visited[100001];
 int dfs(vector<int> adj[], int node, int color){
     visited[node] = color;
     for(auto elem: adj[node]){
-        if(visited[elem]==-1) dfs(adj, elem, color);
+        if(visited[elem]!=color) dfs(adj, elem, color);
     }
 }
 
@@ -13,30 +13,40 @@ int main(){
     int t; cin >> t;
     for(int filler =0; filler<t; filler++){
         int n,m; cin >> n>>m;
-        vector<int> adj[n+1]; 
-        for(int i=0; i<=n+1; i++) visited[i]=-1;
+        vector<int> adj[n]; 
+        for(int i=0; i<n; i++) visited[i]=i;
         for(int i=0; i<m; i++){
             int x,y; cin >>x>>y;
+            x--; y--;
             adj[x].push_back(y); adj[y].push_back(x);
         }
-        int counter=0;
-        for(int i=1; i<=n; i++){
-            if(visited[i]==-1){dfs(adj, i, counter); counter++;}
+        for(int i=0; i<n; i++){
+            if(visited[i]==i) dfs(adj, i, i); 
         }
-        if(counter==1) cout << 0 <<endl;
-        else if(counter==2){
-            if(visited[1]==visited[n]) cout << 0<<endl;
-            else cout << 1<<endl;
-        }else{
-            bool check=false;
-            for(int i=1; i<=n; i++){
-                if(visited[i]==visited[1]){
-                    if(visited[i-1] == visited[n] || visited[i+1]==visited[n]) check=true;
-                }
+
+        vector<vector<int> > coloring(n);
+        for(int i=0; i<n; i++) coloring[visited[i]].push_back(i);
+        vector<long long> storecost(n, 1e9);
+        vector<long long> storecost2(n, 1e9);
+        int p21 = 0;
+        int p22 = 0;
+        for(int i = 0; i < n; i++) {
+            while(p21 < coloring[visited[0]].size()) {
+                storecost[visited[i]] = min(storecost[visited[i]], (long long)abs(i - coloring[visited[0]][p21]));
+                if(coloring[visited[0]][p21] < i) p21++;
+                else break;
             }
-            if(visited[1]==visited[n]) cout << 0<<endl;
-            else if(check) cout << 1<<endl;
-            else cout << 2<<endl;
+            if(p21) p21--;
+            while(p22 < coloring[visited[n-1]].size()) {
+            storecost2[visited[i]] = min(storecost2[visited[i]], (long long)abs(i - coloring[visited[n-1]][p22]));
+            if(coloring[visited[n-1]][p22] < i) p22++;
+            else break;
+            }
+            if(p22) p22--;
         }
+        long long ans=1e18;
+        for(int i = 0; i < n; i++) ans = min(ans, storecost[i]*storecost[i] + storecost2[i]*storecost2[i]);
+        if(visited[0] != visited[n-1]) cout << ans<<endl;
+        else cout << 0<<endl;
     }
 }
