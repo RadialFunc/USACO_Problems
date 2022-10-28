e0c583adee6b8eafec5329ce4301a1e006c1157b
RadialFunc <rohangarg2008@gmail.com>
2022-10-27 21:37:28 -0700

    fixed 2020fg2

--- 2020/Gold/2020FG2.cpp
+++ 2020/Gold/2020FG2.cpp
@@ -12,12 +12,13 @@ int main(){
     ll powersoftwo[n]; powersoftwo[0]=1;
     for(int i=1; i<n; i++) powersoftwo[i] = (powersoftwo[i-1]*2)%MOD;
     map<int, int> count;
+    int pairs[n][2];
     for(int i=0; i<n; i++){
-        int a,b; cin >>a>>b;
-        count[a]++; count[b]--;
+        cin >> pairs[i][0]>>pairs[i][1];
+        count[pairs[i][0]]++; count[pairs[i][1]]--;
     }
-    for(int i=2; i<=2*n; i++) count[i] += count[i-1];
+    for(int i=1; i<=2*n; i++) count[i] += count[i-1];
     ll ans = 0;
-    for(int i=1; i<=2*n; i++) ans = (ans+powersoftwo[n-1-count[i]])%MOD;
+    for(int i=0; i<n; i++) ans = (ans+powersoftwo[n-1-count[pairs[i][0]-1]])%MOD;
     cout << ans;
 }
