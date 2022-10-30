409c072f99705f369a8bbbf16df16150535b08ec
RadialFunc <rohangarg2008@gmail.com>
2022-10-30 13:20:17 -0700

    solved 2016jg1

--- 2016/Gold/2016JG1.cpp
+++ 2016/Gold/2016JG1.cpp
@@ -2,17 +2,56 @@
 using namespace std;
 using ll = long long;
 
+set<ll> x;
+int n;
+
+bool checkleft(ll loc, ll radius){
+    if(loc == *x.begin()) return true;
+    int change = *x.lower_bound(loc-radius);
+    if(change == loc) return false;
+    else return checkleft(change, radius-2);
+}
+
+bool checkright(ll loc, ll radius){
+    if(loc == *x.rbegin()) return true;
+    int change = *(--x.upper_bound(loc+radius));
+    if(change == loc) return false;
+    else return checkright(change, radius-2);
+}
+
 int main(){
-    int n; cin >> n;
-    ll x[n];
-    for(int i=0; i<n; i++){cin >>x[i]; x[i]*=2;}
-    sort(x, x+n);
+    freopen("angry.in", "r", stdin);
+    freopen("angry.out", "w", stdout);
+    cin >> n;
+    for(int i=0; i<n; i++){ll s; cin >> s; x.insert(2*s);}
     ll l=0; ll h= 1e9;
+    ll ans = 1e9;
     while(l!=h){
         ll r = (l+h)/2;
-        ll first = x[0];  ll last = x[n-1];
+        ll first = *x.begin();  ll last = *x.rbegin();
         while(first!= last){
-
+            ll pos = (first+last)/2;
+            bool a = checkleft(pos, r); bool b = checkright(pos,r);
+            if(a==b){
+                if(a) ans = min(ans, r);
+                break;
+            }else{
+                if(a){
+                    if(first==pos) break;
+                    first=pos;
+                }
+                else{
+                    if(last==pos) break;
+                    last=pos;
+                }
+            }
         }
+        if(ans==r){if(h==r) break; h=r;}
+        else{if(l==r) break; l=r;}
+    }
+    if(ans%2==0){
+        cout << ans/2<<".0";
+    }else{
+        cout << ans/2<<".5";
     }
 }
